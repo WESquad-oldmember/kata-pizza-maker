1266eb39c893f2226fc6b2f69926d9d5c3208e9c
spires <spires@wemanity.com>
2020-04-27 10:54:33 +0200

    ✨ Creating pizza maker component

---
+++ src/app/pizza-maker/pizza-maker.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pizza-maker',
+  templateUrl: './pizza-maker.component.html',
+  styleUrls: ['./pizza-maker.component.scss']
+})
+export class PizzaMakerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
