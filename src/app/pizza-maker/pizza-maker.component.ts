0d1ade8a84bd4ce440aeef9acf2a0daf157f9e9e
spires <spires@wemanity.com>
2020-04-29 11:49:23 +0200

    ✨ EstimatedTime of order with toppings

--- src/app/pizza-maker/pizza-maker.component.ts
+++ src/app/pizza-maker/pizza-maker.component.ts
@@ -48,8 +48,13 @@ export class PizzaMakerComponent implements OnInit {
     const bakeMinTime = 10;
     const bakeMaxTime = 15;
     const packagingTime = .25;
+    const toppingsTime = .5;
 
-    const estimatedTime = prepareTime + sauceTime + packagingTime;
+    let estimatedTime = prepareTime + sauceTime + packagingTime;
+
+    if (fakeOrder.pizza.toppings && fakeOrder.pizza.toppings.length > 0) {
+      estimatedTime += toppingsTime;
+    }
 
     return `Between ${Math.trunc(estimatedTime + bakeMinTime)}:${((estimatedTime + bakeMinTime) % 1) * 60}`
       + ` and ${Math.trunc(estimatedTime + bakeMaxTime)}:${((estimatedTime + bakeMaxTime) % 1) * 60}`;
